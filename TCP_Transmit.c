097817ab1b37f39fe976145970df2a142cffe961
Ivo Stratev <ivo_stratev@abv.bg>
2016-04-06 14:26:57 +0300

    Create TCP_Transmit.c

---
+++ TCP_Transmit.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include "TCP_SOCKET_COMMON.h"
+ 
+int main(void) {
+    int sock;
+    struct sockaddr_in server;
+    char message[] = MESG_TEXT;
+    char server_reply[MESG_LENGTH];
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock != -1) {
+        server.sin_addr.s_addr = inet_addr(IP_ADDR);
+        server.sin_family = AF_INET;
+        server.sin_port = htons(SOCKET_PORT);
+        if(connect(sock, (struct sockaddr*) &server , sizeof(struct sockaddr_in)) >= 0) {
+            while(1) {
+                if(send(sock, message, MESG_LENGTH, 0) >= 0) {
+                    if(recv(sock, server_reply, MESG_LENGTH, 0) >= 0) {
+                        puts(server_reply);
+                    }
+                }
+            }
+        }
+    }
+    close(sock);
+    return 0;
+}
