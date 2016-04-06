7cfdac5d2b6607107dbe4ef92f5d1146d451ed47
Ivo Stratev <ivo_stratev@abv.bg>
2016-04-06 14:24:13 +0300

    Create TCP_Recive.c

---
+++ TCP_Recive.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include "TCP_SOCKET_COMMON.h"
+ 
+int main(void) {
+    char client_message[MESG_LENGTH];
+    int socket_desc, client_sock, c, read_size;
+    struct sockaddr_in server, client;
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc != -1) {
+        server.sin_family = AF_INET;
+        server.sin_addr.s_addr = INADDR_ANY;
+        server.sin_port = htons(SOCKET_PORT);
+        if(bind(socket_desc, (struct sockaddr*) &server, sizeof(server)) >= 0) {
+            listen(socket_desc , 3);
+            c = sizeof(struct sockaddr_in);
+            client_sock = accept(socket_desc, (struct sockaddr*) &client, (socklen_t*) &c);
+            if (client_sock >= 0) {
+                while((read_size = recv(client_sock, client_message, MESG_LENGTH, 0)) > 0) {
+                    write(client_sock, client_message, MESG_LENGTH);
+                }
+            }
+        }
+    }
+    return 0;
+}
